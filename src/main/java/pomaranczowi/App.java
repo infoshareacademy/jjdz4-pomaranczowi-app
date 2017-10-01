e9f050699cad581c93aebff9e6b5c8843c029222
matzal <mati.zalewski@gmail.com>
2017-10-01 20:52:35 +0200

    JZ4PF-17, initial commit, download file from URL

--- src/main/java/pomaranczowi/App.java
+++ src/main/java/pomaranczowi/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main( String[] args ) {
 
         System.out.println("Hello world.");
-
+        //ImportCurrentData.downloadFileFromURL(ImportCurrentData.url, ImportCurrentData.file);
     }
 }
---
+++ src/main/java/pomaranczowi/ImportCurrentData.java
@@ -0,0 +1,36 @@
+package pomaranczowi;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class ImportCurrentData {
+
+    static String url = "http://bossa.pl/pub/fundinwest/omega/omegafun.zip";
+    static File file = new File("data/stock_data.zip");
+
+    public static void downloadFileFromURL(String urlString, File destinationFile) {
+        try {
+            URL website = new URL(urlString);
+            ReadableByteChannel readableByteChannel;
+            readableByteChannel = Channels.newChannel(website.openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(destinationFile);
+            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            fileOutputStream.close();
+            readableByteChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
+
