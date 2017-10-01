1f95eebbaf07657220f09500c728e05f3c4c941b
Dawid Śremski <dawid@sremski.com.pl>
2017-10-01 19:29:31 +0200

    JZ4PF-10 hello world

--- src/main/java/pomaranczowi/App.java
+++ src/main/java/pomaranczowi/App.java
@@ -4,9 +4,7 @@ public class App
 {
     public static void main( String[] args ) {
 
-        Currency aud = new Currency("AUD.txt"); //test
-        System.out.println(aud.getClose("20160524")); //test
-        System.out.println(aud.getClose("20160526")); //test
+        System.out.println("Hello world.");
 
     }
 }
