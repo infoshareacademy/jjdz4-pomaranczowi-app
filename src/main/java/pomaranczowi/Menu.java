8ad83b0dc450e3bb6bad431106d04c1073dff846
Dawid Śremski <dawid@sremski.com.pl>
2017-10-02 00:44:23 +0200

    JZ4PF-8 add menu first draft

--- src/main/java/pomaranczowi/Menu.java
+++ src/main/java/pomaranczowi/Menu.java
@@ -1,5 +1,33 @@
 package pomaranczowi;
 
+import java.util.ArrayList;
+
+
 public class Menu {
 
+    ArrayList<String> list = new ArrayList<>();
+    // TODO: 02.10.17 change ArrayList to work on MenuPosition objects
+
+    public void add(String label) {
+        list.add(label);
+    }
+
+    public int Show() {
+        System.out.println("||| ANALIZATOR FINANSOWY ||||\n");
+        for (String menuPosition : list) {
+            // TODO: 02.10.17 complete the loop to print out all menu positions
+        }
+
+        MenuPosition menu1 = new MenuPosition("Waluta");
+        MenuPosition menu2 = new MenuPosition("Giełda");
+        MenuPosition menu3 = new MenuPosition("Wyjście");
+
+        menu1.Show();
+        menu2.Show();
+        menu3.Show();
+
+        System.out.println();
+
+        return 0;
+    }
 }
---
+++ src/main/java/pomaranczowi/MenuPosition.java
@@ -0,0 +1,28 @@
+package pomaranczowi;
+
+public class MenuPosition {
+
+    static int counter;
+    private String label;
+    private Boolean active;
+    private int id;
+
+    public MenuPosition(String label) {
+        id = counter;
+        this.label = "( ) "+label;
+        counter++;
+    }
+
+    public void setLabel(String label) {
+        this.label = "( ) "+label;
+    }
+
+    public void Active(boolean isactive) {
+        this.active = isactive;
+    }
+
+    public void Show() {
+        System.out.println(label);
+    }
+
+}
