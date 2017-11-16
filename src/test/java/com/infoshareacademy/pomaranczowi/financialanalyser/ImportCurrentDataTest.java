d652b05d19d844dd376f34335f48a3de6fa5eebb
matzal <mati.zalewski@gmail.com>
2017-11-16 20:11:52 +0100

    JZ4PF-34 tests initial commit

---
+++ src/test/java/com/infoshareacademy/pomaranczowi/financialanalyser/ImportCurrentDataTest.java
@@ -0,0 +1,15 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImportCurrentDataTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+
+}
