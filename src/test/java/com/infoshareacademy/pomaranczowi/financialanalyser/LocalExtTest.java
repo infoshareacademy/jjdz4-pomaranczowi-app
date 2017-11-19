59b4ac9654d2dd7a52f749135d00cc9d89a0f039
tomekokoniewski <alai@wp.pl>
2017-11-19 19:48:04 +0100

    JZ4PF-32-test_extremes initial commit

---
+++ src/test/java/com/infoshareacademy/pomaranczowi/financialanalyser/LocalExtTest.java
@@ -0,0 +1,26 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser;
+
+import org.junit.Test;
+
+public class LocalExtTest {
+    @Test
+    public void showAll() throws Exception {
+    }
+
+    @Test
+    public void getStartDate() throws Exception {
+    }
+
+    @Test
+    public void getEndDate() throws Exception {
+    }
+
+    @Test
+    public void setStartDate() throws Exception {
+    }
+
+    @Test
+    public void setEndDate() throws Exception {
+    }
+
+}
