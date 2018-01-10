e9c57f18a8a21aabedf540941217712757bd2177
Dawid Śremski <dawid.sremski@gmail.com>
2018-01-10 17:08:37 +0100

    JZ4PF-26 Add CallbackServlet

---
+++ webapp/src/main/java/com/infoshareacademy/pomaranczowi/financialanalyser/servlets/CallbackServlet.java
@@ -0,0 +1,59 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser.servlets;
+
+import com.auth0.AuthenticationController;
+import com.auth0.IdentityVerificationException;
+import com.auth0.SessionUtils;
+import com.auth0.Tokens;
+import com.infoshareacademy.pomaranczowi.financialanalyser.AuthenticationControllerProvider;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+@WebServlet(urlPatterns = {"/callback"})
+public class CallbackServlet extends HttpServlet {
+
+    private String redirectOnSuccess;
+    private String redirectOnFail;
+    private AuthenticationController authenticationController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        redirectOnSuccess = "/home";
+        redirectOnFail = "/";
+
+        try {
+            authenticationController = AuthenticationControllerProvider.getInstance(config);
+        } catch (UnsupportedEncodingException e) {
+            throw new ServletException("Couldn't create the AuthenticationController instance. Check the configuration.", e);
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        handle(req, res);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        handle(req, res);
+    }
+
+    private void handle(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        try {
+            Tokens tokens = authenticationController.handle(req);
+            SessionUtils.set(req, "accessToken", tokens.getAccessToken());
+            SessionUtils.set(req, "idToken", tokens.getIdToken());
+            res.sendRedirect(redirectOnSuccess);
+        } catch (IdentityVerificationException e) {
+            e.printStackTrace();
+            res.sendRedirect(redirectOnFail);
+        }
+    }
+}
