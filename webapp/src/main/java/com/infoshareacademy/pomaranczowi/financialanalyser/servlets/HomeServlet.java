fbbbc0223f2a160c03348fd6ec4714b301bf3ccd
matzal <mati.zalewski@gmail.com>
2018-01-25 15:42:13 +0100

    edit step 1 - fund/currency list instead of text input - not finished

--- webapp/src/main/java/com/infoshareacademy/pomaranczowi/financialanalyser/servlets/HomeServlet.java
+++ webapp/src/main/java/com/infoshareacademy/pomaranczowi/financialanalyser/servlets/HomeServlet.java
@@ -13,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/portal/home")
 public class HomeServlet extends HttpServlet {
@@ -26,8 +28,8 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/portal/home.jsp");
-        request.setAttribute("step",0);
-        requestDispatcher.forward(request,response);
+        request.setAttribute("step", 0);
+        requestDispatcher.forward(request, response);
     }
 
     @Override
@@ -35,11 +37,13 @@ public class HomeServlet extends HttpServlet {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/portal/home.jsp");
 
         Integer step = Integer.valueOf(request.getParameter("step"));
-        request.getSession().setAttribute("step",step);
+        request.getSession().setAttribute("step", step);
 
         if (step == 1) {
             if (request.getParameter("data") != null) {
                 request.getSession().setAttribute("data", request.getParameter("data"));
+                List<String> waluty = Arrays.asList("USD", "EUR", "AUD");
+                request.getSession().setAttribute("listOfCodes", waluty);
             }
         }
 
@@ -52,16 +56,22 @@ public class HomeServlet extends HttpServlet {
         if (step == 3) {
             if (request.getParameter("action") != null) {
                 request.getSession().setAttribute("action", request.getParameter("action"));
-                printPricesIfGlobalExtremes(request);
+                String code = (String) request.getSession().getAttribute("code");
+                switch (request.getParameter("action")) {
+                    case "globalExtremes":
+                        printPricesForGlobalExtremes(request, code);
+                        break;
+                }
             }
         }
 
-        requestDispatcher.forward(request,response);
+        requestDispatcher.forward(request, response);
     }
 
-    private void printPricesIfGlobalExtremes(HttpServletRequest request) {
-        if(request.getParameter("action").equals("globalExtremes")){
-            request.getSession().setAttribute("maxOpen", 10);
-        }
+    private void printPricesForGlobalExtremes(HttpServletRequest request, String code) {
+        request.getSession().setAttribute("maxOpen",
+                priceRepositoryDao.getMaxOpenFromDateToDate(code,
+                        LocalDate.parse("2010-01-20", DateTimeFormatter.ISO_DATE),
+                        LocalDate.parse("2018-01-20", DateTimeFormatter.ISO_DATE)));
     }
 }
--- webapp/src/main/webapp/portal/form-step-1.jsp
+++ webapp/src/main/webapp/portal/form-step-1.jsp
@@ -3,12 +3,18 @@
 <form id="form" action="/portal/home" method="post" novalidate>
     <div class="form-group row">
         <div class="col-sm-10">
-            <label for="chooseCode">Podaj kod
+            <label for="chooseCode">Wybierz kod
                 <c:out value="${data == 'fund' ? ' funduszu inwestycyjnego, którego ': ' waluty, której '}"/>
                 analizy chcesz dokonać:
             </label>
-            <input type="text" class="form-control" name="code" id="chooseCode"
-                   value="<c:out value="${sessionScope.code}"/>" required>
+            <%--<input type="text" class="form-control" name="code" id="chooseCode"
+                   value="<c:out value="${sessionScope.code}"/>" required>--%>
+            <select type="text" class="form-control" name="code" id="chooseCode" required>
+                <c:forEach var="CodeFromDB" items="${listOfCodes}">
+                    <option <c:if test="${CodeFromDB == sessionScope.code}">selected</c:if>>
+                        <c:out value="${CodeFromDB}"/></option>
+                </c:forEach>
+            </select>
             <div class="invalid-feedback">
                 Proszę podać kod <c:out value="${data == 'fund' ? 'funduszu inwestycyjnego': 'waluty'}"/>.
             </div>
