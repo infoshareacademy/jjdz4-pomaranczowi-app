0a0c8e803995e5afdb03a628c564ad86c25a48e9
Mateusz Sosnowski <mateusz.sosnowski@logintegra.com>
2018-01-29 21:13:54 +0100

    JZ4PF-35 Initial commit Fund Investment Tests

---
+++ cli/src/test/java/com/infoshareacademy/pomaranczowi/financialanalyser/financial/investment/InvestmentNameTest.java
@@ -0,0 +1,20 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser.financial.investment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvestmentNameTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void loadInvestmentNameFromFile() throws Exception {
+    }
+
+}
