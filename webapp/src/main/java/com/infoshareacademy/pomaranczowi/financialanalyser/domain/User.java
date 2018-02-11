3c0a3f039ebaaa43c3fcb8d0690dcf1dff532b15
Dawid Śremski <dawid@sremski.com.pl>
2018-02-11 16:53:01 +0100

    Create user class, add authorization scope

---
+++ webapp/src/main/java/com/infoshareacademy/pomaranczowi/financialanalyser/domain/User.java
@@ -0,0 +1,38 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class User {
+
+    @JsonIgnore
+    String sub;
+
+    @JsonProperty("given_name")
+    String givenName;
+
+    @JsonProperty("family_name")
+    String familyName;
+
+    String nickname;
+
+    String name;
+
+    String picture;
+
+    String gender;
+
+    String locale;
+
+    @JsonIgnore
+    @JsonProperty("updated_at")
+    String updatedAt;
+
+    String email;
+
+    @JsonIgnore
+    @JsonProperty("email_verified")
+    String emailVerified;
+}
