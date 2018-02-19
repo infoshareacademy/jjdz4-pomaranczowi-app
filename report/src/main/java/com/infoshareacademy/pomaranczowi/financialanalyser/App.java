553120409cb755530d57684491f83cdf9b632d19
Mateusz Sosnowski <mateusz.sosnowski@logintegra.com>
2018-02-19 23:46:40 +0100

    Add maven dependency

---
+++ report/src/main/java/com/infoshareacademy/pomaranczowi/financialanalyser/App.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class App extends Application {
+
+}
