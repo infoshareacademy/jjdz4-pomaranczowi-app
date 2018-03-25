2669d7b905c8fc7e60ca9cfb2bed37037279fff0
matzal <mati.zalewski@gmail.com>
2018-03-25 16:42:54 +0200

    make homepage button work

---
+++ webapp/src/main/java/com/infoshareacademy/pomaranczowi/financialanalyser/servlets/GoToHomePage.java
@@ -0,0 +1,18 @@
+package com.infoshareacademy.pomaranczowi.financialanalyser.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/homepage")
+public class GoToHomePage extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().setAttribute("step", 0);
+        resp.sendRedirect("/portal/home");
+    }
+}
